ce09d15a17c85230dc89d8f61146652435774bc1
guilinhd <394811364@qq.com>
2022-01-29 09:47:27 +0800

    新增student相关模型

---
+++ src/MockSchoolManagement/DataRepositories/IStudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MockSchoolManagement.Models;
+
+namespace MockSchoolManagement.DataRepositories
+{
+    interface IStudentRepository
+    {
+        Student GetStudent(int id);
+    }
+}
---
+++ src/MockSchoolManagement/DataRepositories/MockStudentRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MockSchoolManagement.Models;
+
+namespace MockSchoolManagement.DataRepositories
+{
+    public class MockStudentRepository : IStudentRepository
+    {
+        List<Student> _students = new List<Student>();
+
+        public MockStudentRepository()
+        {
+            _students.Add(
+                new Student() { 
+                    Id = 1,
+                    Name = "张三",
+                    Email = "zhangsan@hotmail.com"
+                },
+                new Student() { 
+                    Id = 2,
+                    Name = "李四",
+                    Email = "lisi@hotmail.com"
+                });
+        }
+
+        public Student GetStudent(int id)
+        {
+            Student student = _students.Where(f => f.Id == 1).FirstOrDefault();
+
+            return student;
+        }
+    }
+}

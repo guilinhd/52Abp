63e34194b8d8651a8471eeb3e58e3c5713c48893
guilinhd <394811364@qq.com>
2022-02-10 10:38:36 +0800

    增加用户管理控制器

--- src/MockSchoolManagement/Controllers/UserController.cs
+++ src/MockSchoolManagement/Controllers/UserController.cs
@@ -34,8 +34,8 @@ namespace MockSchoolManagement.Controllers
             var user =  await _userManager.FindByIdAsync(id);
             if (user == null)
             {
-                ViewBag.ErrorMessage = "用户Id：{id} 的信息不存在!";
-                return RedirectToAction("NoFound", "Error");
+                ViewBag.ErrorMessage = $"用户Id：{id} 的信息不存在!";
+                return RedirectToAction("NoFound");
             }
 
             UserUpdateViewModel model = new UserUpdateViewModel()
@@ -54,6 +54,37 @@ namespace MockSchoolManagement.Controllers
             var roles = await _userManager.GetRolesAsync(user);
             model.Roles = roles.ToList();
             
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UserUpdateViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.Id);
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"用户Id：{model.Id} 的信息不存在!";
+                return RedirectToAction("NoFound");
+            }
+
+            if (ModelState.IsValid)
+            {
+                user.Email = model.Email;
+                user.UserName = model.Name;
+                user.City = model.City;
+
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "User");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
             return View(model);
         }
     }
